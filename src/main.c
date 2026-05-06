e364d12b44cfbe8a8dd063f6e6a043b03d428a69
Saransivakumar89 <saransivakumar89@gmail.com>
2026-05-06 20:37:17 +0530

    feat: add the basic shell cmd to execute

--- src/main.c
+++ src/main.c
@@ -9,19 +9,50 @@ int main(int argc, char **argv){
         return 1;
     }
 
-
-
     while(1){
         printf("shell-lite>> ");
 
         if(fgets(buff, sizeof(buff), stdin) == NULL){
-            break; //EOF
+            break;
         }
 
-        // Removes the newline
         buff[strcspn(buff, "\n")] = '\0';
 
-        printf("You entered: %s\n", buff);
+	//exit the shell-lite
+        if(strcmp(buff,"exit")==0){
+            break;
+        }
+	
+	//catch the newline
+        if(buff[0]=='\0'){
+            continue;
+        }
+
+        // tokenizes the inputs
+        char *args[10];
+        int i = 0;
+
+        char *token = strtok(buff, " ");
+        while (token != NULL && i < 9) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
+
+        //creates the child process and executes the process
+        pid_t pid = fork();
+
+        if(pid < 0){
+            perror("fork");
+        }
+        else if(pid == 0){
+            execvp(args[0], args);
+            perror("execvp");
+            exit(1);
+        }
+        else {
+            wait(NULL);
+        }
     }
 
     return 0;
